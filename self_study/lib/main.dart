c5ec0d32f1302acff64f267b9f2e3227d6f38b29
Chinapanda <kittikornchina2014@gmail.com>
2021-01-31 21:30:51 +0700

    My first app

---
+++ self_study/lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var questions = [
+    {
+      'questionText': 'What\'s your favorite Country?',
+      'answers': ['Thai', 'Japanese', 'USA', 'England'],
+    },
+    {
+      'questionText': 'What\'s your favorite movives?',
+      'answers': ['fast&furious', 'Mission Impossible', 'Disney', 'Marvel'],
+    },
+    {
+      'questionText': 'What\'s your favorite sport?',
+      'answers': ['Football', 'Basketball', 'Golf', 'Chairball'],
+    },
+  ];
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
+      ),
+    );
+  }
+}
