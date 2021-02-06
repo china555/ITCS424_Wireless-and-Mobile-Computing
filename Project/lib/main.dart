a42608ede50ce6691cf7ee8997ba12c0ee58f0fe
Chinapanda <kittikornchina2014@gmail.com>
2021-02-06 22:13:29 +0700

    update CustomScrollView & Slivers In Flutter

--- Project/lib/main.dart
+++ Project/lib/main.dart
@@ -9,72 +9,165 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(182, 54, 39, 1),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 30),
-                child: Text(
-                  'A WHOLE\nNEW\nWORD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'IndieFlower',
-                    fontSize: 64,
-                    color: Colors.white,
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 30),
+                  child: Text(
+                    'A WHOLE\nNEW\nWORD',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'IndieFlower',
+                      fontSize: 64,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 320,
-                height: 200,
-                // alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/sport.jpg'),
-                      fit: BoxFit.fitWidth),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 320,
+                  height: 200,
+                  // alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/movie.jpg'),
+                        fit: BoxFit.fitWidth),
+                  ),
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0.0),
+                    onPressed: () {
+                      print('Hello');
+                    },
+                  ),
                 ),
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    print('Hello');
-                  },
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 320,
+                  height: 200,
+                  margin: EdgeInsets.only(top: 30),
+                  // alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/sport.jpg'),
+                        fit: BoxFit.fitWidth),
+                  ),
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0.0),
+                    onPressed: () {
+                      print('Hello');
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 320,
-                height: 300,
-                // alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/sport.jpg'),
-                      fit: BoxFit.fitWidth),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 320,
+                  height: 200,
+                  margin: EdgeInsets.only(top: 30),
+                  // alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/food.jpg'),
+                        fit: BoxFit.fitWidth),
+                  ),
+                  child: FlatButton(
+                    padding: EdgeInsets.all(0.0),
+                    onPressed: () {
+                      print('Hello');
+                    },
+                  ),
                 ),
-                child: FlatButton(
-                  padding: EdgeInsets.all(0.0),
-                  onPressed: () {
-                    print('Hello');
-                  },
-                ),
-              ),
-            ],
-          )
+              ],
+            ),
+          ]))
         ],
       ),
     );
   }
 }
+// Column(
+//       children: [
+// Row(
+//   mainAxisAlignment: MainAxisAlignment.center,
+//   crossAxisAlignment: CrossAxisAlignment.start,
+//   children: <Widget>[
+//     Container(
+//       margin: EdgeInsets.only(top: 30),
+//       child: Text(
+//         'A WHOLE\nNEW\nWORD',
+//         textAlign: TextAlign.center,
+//         style: TextStyle(
+//           fontFamily: 'IndieFlower',
+//           fontSize: 64,
+//           color: Colors.white,
+//         ),
+//       ),
+//     ),
+//   ],
+// ),
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Container(
+//               width: 320,
+//               height: 200,
+//               // alignment: Alignment.center,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(20),
+//                 image: DecorationImage(
+//                     image: AssetImage('assets/images/sport.jpg'),
+//                     fit: BoxFit.fitWidth),
+//               ),
+//               child: FlatButton(
+//                 padding: EdgeInsets.all(0.0),
+//                 onPressed: () {
+//                   print('Hello');
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//         Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Container(
+//               width: 320,
+//               height: 300,
+//               // alignment: Alignment.center,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(20),
+//                 image: DecorationImage(
+//                     image: AssetImage('assets/images/sport.jpg'),
+//                     fit: BoxFit.fitWidth),
+//               ),
+//               child: FlatButton(
+//                 padding: EdgeInsets.all(0.0),
+//                 onPressed: () {
+//                   print('Hello');
+//                 },
+//               ),
+//             ),
+//           ],
+//         )
+//       ],
+//     ),
