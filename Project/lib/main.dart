8d7a530e57eab8bfecb4bb469a7ac38497f696b3
Chinapanda <kittikornchina2014@gmail.com>
2021-02-06 18:12:16 +0700

    fix the UI

--- Project/lib/main.dart
+++ Project/lib/main.dart
@@ -9,23 +9,63 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(182, 54, 39, 1),
-      body: Container(
-        alignment: Alignment(0.0, -0.8),
-        child: Row(
+      body: Column(
+        children: [
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(
-                'A WHOLE\nNEW\nWORD',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'IndieFlower',
-                  fontSize: 64,
-                  color: Colors.white,
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: Text(
+                  'A WHOLE\nNEW\nWORD',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'IndieFlower',
+                    fontSize: 64,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ]),
+            ],
+          ),
+          Row(
+            children: [
+              FlatButton(
+                onPressed: null,
+                child: Center(
+                  child: Image.asset('assets/images')
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
 }
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color.fromRGBO(182, 54, 39, 1),
+//       body: Container(
+//         alignment: Alignment(0.0, -0.8),
+//         child: Row(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: <Widget>[
+//               Text(
+//                 'A WHOLE\nNEW\nWORD',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   fontFamily: 'IndieFlower',
+//                   fontSize: 64,
+//                   color: Colors.white,
+//                 ),
+//               ),
+//             ]),
+//       ),
+//     );
+//   }
+// }
