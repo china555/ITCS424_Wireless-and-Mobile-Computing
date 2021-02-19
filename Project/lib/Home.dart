ccc8f7551ba54c58accefdddae5294f83cf3c663
Chinapanda <kittikornchina2014@gmail.com>
2021-02-19 23:30:48 +0700

    home page

--- Project/lib/Home.dart
+++ Project/lib/Home.dart
@@ -6,29 +6,57 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(26, 35, 126, 1),
-      body: ListView(
-        itemExtent: 265,
+      body: Column(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(bottom: 30),
-                child: Text(
-                  'A WHOLE\nNEW\nWORD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontFamily: 'IndieFlower',
-                      fontSize: 60,
-                      color: Colors.white,
-                      height: 1.3),
-                ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Text(
+                      'A WHOLE\nNEW\nWORD',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontFamily: 'IndieFlower',
+                          fontSize: 60,
+                          color: Colors.white,
+                          height: 1.3),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-          SelectCardGame()
+          Expanded(child: SelectCardGame())
         ],
       ),
+      // body: ListView(
+      //   itemExtent: 265,
+      //   children: [
+      //     Column(
+      //       crossAxisAlignment: CrossAxisAlignment.center,
+      //       children: <Widget>[
+      //         Container(
+      //           margin: EdgeInsets.only(bottom: 30),
+      //           child: Text(
+      //             'A WHOLE\nNEW\nWORD',
+      //             textAlign: TextAlign.center,
+      //             style: TextStyle(
+      //                 fontFamily: 'IndieFlower',
+      //                 fontSize: 60,
+      //                 color: Colors.white,
+      //                 height: 1.3),
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+
+      //     SelectCardGame()
+      //   ],
+      // ),
     );
   }
 }
