c4af3e07fe8bc1efbf62a665782b84b4e93f9d33
Chinapanda <kittikornchina2014@gmail.com>
2021-02-19 19:27:19 +0700

    adding

--- Project/lib/main.dart
+++ Project/lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'Home.dart';
 
 void main() => runApp(MaterialApp(
+      title: 'A WHOLE NEW WORD',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Home(),
       debugShowCheckedModeBanner: false,
     ));
