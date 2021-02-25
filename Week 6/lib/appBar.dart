f353714912068e579879abc098f064c13ee3afb3
Chinapanda <kittikornchina2014@gmail.com>
2021-02-26 02:17:53 +0700

    update appBar

--- Week 6/lib/appBar.dart
+++ Week 6/lib/appBar.dart
@@ -4,21 +4,22 @@ import 'shopping_cart.dart';
 class MyAppBar extends AppBar {
   MyAppBar(BuildContext context)
       : super(
-            elevation: 0,
-            backgroundColor: Colors.white,
-            leading: Icon(
-              Icons.menu,
-              color: Colors.black,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.shopping_cart_outlined, color: Colors.black),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ShoppingCart()),
+                );
+              },
             ),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.shopping_cart_outlined, color: Colors.black),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ShoppingCart()),
-                  );
-                },
-              ),
-            ]);
+          ],
+        );
 }
