c3a1f07cba9b73eeb97ee805bd9ec68e53fb41c4
Chinapanda <kittikornchina2014@gmail.com>
2021-02-25 23:04:33 +0700

    Wow

--- Week 6/lib/shopping_cart.dart
+++ Week 6/lib/shopping_cart.dart
@@ -2,8 +2,32 @@ import 'package:flutter/material.dart';
 import 'appBar.dart';
 import 'constants.dart';
 
-class Shopping_cart extends StatelessWidget {
+class ShoppingCart extends StatefulWidget {
   @override
+  _ShoppingCartState createState() => _ShoppingCartState();
+}
+
+class _ShoppingCartState extends State<ShoppingCart> {
+  @override
+  List<Widget> itemsData = [];
+
+  void getPostsData() {
+    List<dynamic> responseList = cart;
+    List<Widget> listItems = [];
+    responseList.forEach((post) {
+      listItems.add();
+    });
+    setState(() {
+      itemsData = listItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData();
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
