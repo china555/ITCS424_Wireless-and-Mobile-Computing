459d7ab95ce55e181d2c615ad236090c7895b8b7
Chinapanda <kittikornchina2014@gmail.com>
2021-03-06 11:50:36 +0700

    add data from express

--- Week 7/backend/src/index.js
+++ Week 7/backend/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParse = require("body-parser");
 const app = express();
 
 app.use(
@@ -11,7 +10,34 @@ app.use(
 app.use(cors());
 
 app.get("/", async (req, res) => {
-  res.send({ name: "china" });
+  res.send([
+    { name: "PUU AAD", brand: "NONGNAM", price: 10, image: "1.png" },
+    { name: "KUNG MANG KORN", brand: "NONGNAM", price: 50, image: "2.jpg" },
+    { name: "SAA RAAI", brand: "NONGNAM", price: 20, image: "3.png" },
+    { name: "SALMON NOT BURN~~~", brand: "NONGNAM", price: 30, image: "4.png" },
+    { name: "MAENG KRA PRUNE", brand: "NONGNAM", price: 20, image: "5.png" },
+    {
+      name: "FISH SLIDE GRILL CEE EEL",
+      brand: "NONGNAM",
+      price: 50,
+      image: "6.png",
+    },
+    { name: "BANGKOK ROLL", brand: "NONGNAM", price: 200, image: "7.jpg" },
+    { name: "SWEET BALLS", brand: "NONGNAM", price: 10000, image: "8.jpg" },
+    {
+      name: "BANGKOK SALMON GONE",
+      brand: "NONGNAM",
+      price: 200,
+      image: "9.jpg",
+    },
+    { name: "KUNG BALLS", brand: "NONGNAM", price: 30, image: "10.jpg" },
+    {
+      name: "SALMON BINGSUU",
+      brand: "NONGNAM",
+      price: 150000,
+      image: "11.jpg",
+    },
+  ]);
 });
 
 app.listen(8000, () => {
