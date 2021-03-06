1f0cd727a1366968c6f5f44635bf38bb6665a31e
Chinapanda <kittikornchina2014@gmail.com>
2021-03-06 20:43:11 +0700

    add UI of playing game page

--- Project/lib/guess.dart
+++ Project/lib/guess.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
 
 class GameGuessWord extends StatefulWidget {
   @override
@@ -8,6 +9,44 @@ class GameGuessWord extends StatefulWidget {
 class _GameGuessWordState extends State<GameGuessWord> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.indigo[900],
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(
+                top: (MediaQuery.of(context).size.height) / 2 - 60),
+            child: Text(
+              'FootBall',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            margin:
+                EdgeInsets.only(top: (MediaQuery.of(context).size.height / 8)),
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.amber[700],
+                ),
+                child: Text(
+                  '60',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
