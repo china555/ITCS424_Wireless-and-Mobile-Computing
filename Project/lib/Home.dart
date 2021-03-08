658f05c0a6a9ac9c9c62bd45a582f5f8f6c7af41
Chinapanda <kittikornchina2014@gmail.com>
2021-03-09 00:26:29 +0700

    adding portraitUp&Down

--- Project/lib/Home.dart
+++ Project/lib/Home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'Card.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return Scaffold(
       backgroundColor: Color.fromRGBO(26, 35, 126, 1),
       body: Column(
