26776fabd007099c9cec29467f150c6bcda54588
Chinapanda <kittikornchina2014@gmail.com>
2021-03-08 23:57:20 +0700

    adding orientation and sensor

--- Project/lib/guess.dart
+++ Project/lib/guess.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:sensors/sensors.dart';
+import 'package:flutter/services.dart';
 
 class GameGuessWord extends StatefulWidget {
+  List words = [];
+  GameGuessWord(this.words);
   @override
-  _GameGuessWordState createState() => _GameGuessWordState();
+  _GameGuessWordState createState() => _GameGuessWordState(words);
 }
 
 class _GameGuessWordState extends State<GameGuessWord> {
+  Color _backgroundColor = Colors.indigo;
+  List words = [];
+  int index = 0;
+  _GameGuessWordState(this.words);
+  @override
+  void initState() {
+    super.initState();
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      bool test = true;
+      if (event.z < 3 || event.z > -3) {
+        test = false;
+      }
+      setState(() {
+        // Manipulate the UI here, something like:
+        print(words[index]);
+        if (event.z < -9) {
+          _backgroundColor = Colors.green;
+          if (event.z == 0) {
+            index++;
+          }
+        } else if (event.z > 9) {
+          _backgroundColor = Colors.red;
+          if (event.z == 0) {
+            index++;
+          }
+        } else {
+          _backgroundColor = Colors.indigo;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
     return Scaffold(
-      backgroundColor: Colors.indigo[900],
+      backgroundColor: _backgroundColor,
       body: Column(
         // mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -19,7 +58,7 @@ class _GameGuessWordState extends State<GameGuessWord> {
             margin: EdgeInsets.only(
                 top: (MediaQuery.of(context).size.height) / 2 - 60),
             child: Text(
-              'FootBall',
+              "${words[index]}",
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 100,
